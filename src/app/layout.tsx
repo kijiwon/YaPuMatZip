9d686e1382245f2c7bae2ebed976ed500bef475d
kijiwon <kiji1313@gmail.com>
2024-11-16 01:33:50 +0900

    kakaomap sdk url 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
       <Script src="https://developers.kakao.com/sdk/js/kakao.js" async />
       <Script
         type="text/javascript"
-        src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_API_KEY}`}
+        src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_API_KEY}&libraries=services,clusterer`}
       />
     </html>
   );
