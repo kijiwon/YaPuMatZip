be7d84602c4edff3fa42ae6e35a7dfc06bfcd2ff
kijiwon <kiji1313@gmail.com>
2024-11-15 14:10:32 +0900

    tailwind custom theme 설정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,13 @@
-"use client";
-
-import dynamic from "next/dynamic";
-
-const DynamicRecord = dynamic(() => import("./Record"), { ssr: false });
+import "./globals.css";
 
 export default function Home() {
   return (
-    <div>
-      Home
-      <DynamicRecord />
+    <div className="  h-[100vh] bg-ball-park opacity-70  sm:bg-cover lg:bg-center ">
+      <h1 className=" text-[50px] font-bold text-white font-kbo ">
+        야구장
+        <br />
+        맛ZIP
+      </h1>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,37 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+        "main-blue": "#002561",
+        "main-red": "#ed1c23",
+        "kia-red": "#EA0029",
+        "kia-black": "#06141F",
+        "samsung-blue": "#074CA1",
+        "samsung-gray": "#C0C0C0",
+        "lg-red": "#a50034",
+        "doosan-navy": "#131230",
+        "doosan-red": "#ED1C24",
+        "ssg-red": "#ce0e2d",
+        "ssg-yellow": "#ffb81c",
+        "lotte-red": "#D00F31",
+        "lotte-blue": "#041E42",
+        "hanwha-orange": "#FF6600",
+        "hanwha-black": "#020F17",
+        "nc-dip-blue": "#071d3d",
+        "nc-blue": "#1d477d",
+        "nc-gold": "#c7a079",
+        "kiwoom-burgundy": "#570514",
+      },
+      backgroundImage: {
+        "ball-park": "url(/ballpark.png)",
+      },
+      fontFamily: {
+        kbo: "KBO-Dia-Gothic_bold",
+      },
+    },
   },
   plugins: [],
 };
