7649d8c22d20a25141753fa4575189f7fe5e5411
kijiwon <kiji1313@gmail.com>
2024-11-19 22:27:21 +0900

    marker표시

--- src/app/components/KakaoMap.tsx
+++ src/app/components/KakaoMap.tsx
@@ -1,33 +1,62 @@
 "use client";
-
 import { useState } from "react";
+import { useRef } from "react";
 import { useEffect } from "react";
-import { Map } from "react-kakao-maps-sdk";
 
 export default function KakaoMap() {
+  const mapRef = useRef(null);
   const [isLoading, setIsLoading] = useState<boolean>(false);
 
   useEffect(() => {
     const script: HTMLScriptElement = document.createElement("script");
     script.async = true;
-    script.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_API_KEY}&autoload=false`;
-    document.head.appendChild(script);
+    script.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_API_KEY}&libraries=services&autoload=false`;
+    document.body.appendChild(script);
+    const onLoadKakaoMap = () => {
+      window.kakao.maps.load(() => {
+        const mapContainer: HTMLElement = document.getElementById("map")!;
+        const mapOption = {
+          center: new window.kakao.maps.LatLng(37.3, 127.01), // 지도의 중심좌표
+          level: 4, // 지도의 확대 레벨
+        };
+        const map = new window.kakao.maps.Map(mapContainer, mapOption);
+        const ps = new kakao.maps.services.Places(map);
 
+        ps.categorySearch(
+          "FD6",
+          (data, status) => {
+            if (status === kakao.maps.services.Status.OK) {
+              console.log(data);
+
+              for (let i = 0; i < data.length; i++) {
+                const marker = new kakao.maps.Marker({
+                  map: map,
+                  position: new kakao.maps.LatLng(
+                    Number(data[i].y),
+                    Number(data[i].x)
+                  ),
+                  title: data[i].place_name,
+                });
+
+                marker.setMap(map);
+              }
+            }
+          },
+          { useMapBounds: true }
+        );
+      });
+    };
     script.addEventListener("load", () => {
+      onLoadKakaoMap();
       setIsLoading(true);
     });
   }, []);
-
   return (
     <div>
       {isLoading ? (
-        <Map
-          center={{ lat: 33.5563, lng: 126.79581 }}
-          className=" w-[500px] h-[360px]"
-          level={3}
-        />
+        <div ref={mapRef} id="map" className=" w-[500px] h-[350px]"></div>
       ) : (
-        <div>Loading...</div>
+        <div>loading...</div>
       )}
     </div>
   );
