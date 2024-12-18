7ee9e7ff5cb2f7191bd8ada213084bf6bcde0a50
kijiwon <kiji1313@gmail.com>
2024-12-18 23:21:10 +0900

    PlaceInfo 컴포넌트 분리

---
+++ src/app/stadium/[id]/[place]/PlaceInfo.tsx
@@ -0,0 +1,111 @@
+"use client";
+
+import { useRecommededMenusData } from "@/app/hooks/useRecommendedMenusData";
+import { useYapuPlaceDatailData } from "@/app/hooks/useYapuPlaceData";
+import BackButton from "@/components/BackButton";
+import KakaoMap from "@/components/KakaoMap";
+import { usePlaceStore } from "@/stores/place-store";
+import { useStadiumStore } from "@/stores/stadium-store";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+
+export default function PlaceInfo() {
+  const [isClickedBack, setIsClickedBack] = useState(false);
+  const { selectedStadium } = useStadiumStore();
+  const { selectedPlace, clearSelectedPlace } = usePlaceStore();
+  const hasHydrated = usePlaceStore.persist.hasHydrated();
+
+  const router = useRouter();
+  const { isPlaceLoading, yapuPlaceDetailData } = useYapuPlaceDatailData(
+    selectedStadium?.id as string,
+    selectedPlace!
+  );
+
+  const { isMenuLoading, recommendedMenusData } = useRecommededMenusData(
+    selectedPlace!
+  );
+
+  const handleBackButton = () => {
+    clearSelectedPlace();
+    router.replace(`/stadium/${selectedStadium?.id}`);
+  };
+
+  // 브라우저 뒤로가기 제어
+  useEffect(() => {
+    const handlePopState = () => {
+      console.log("뒤로가기 감지");
+      setIsClickedBack(true);
+      clearSelectedPlace();
+    };
+    window.addEventListener("popstate", handlePopState);
+    setIsClickedBack(false);
+  }, [isClickedBack]);
+
+  if (isPlaceLoading || isMenuLoading) {
+    return <div>loading...</div>;
+  }
+
+  if (!hasHydrated) return;
+
+  return (
+    <div>
+      <BackButton fn={handleBackButton} />
+      <div>
+        {yapuPlaceDetailData[0] && (
+          <div className="flex flex-col">
+            <section className="mb-[20px]">
+              <div>
+                <h1 className="flex flex-row justify-center items-center font-paper_logy text-[32px] text-center">
+                  <span className="mr-[5px]">
+                    {yapuPlaceDetailData[0]?.food_type}
+                  </span>
+                  <span>{yapuPlaceDetailData[0]?.name}</span>
+                </h1>
+              </div>
+              <p className="font-s_core font-bold text-[18px] mt-[30px] mb-[10px]">
+                위치 : {yapuPlaceDetailData[0]?.location}
+              </p>
+              <p className="font-s_core flex text-[18px] flex-row items-center mb-[10px]">
+                <span className="mr-[20px]">
+                  {yapuPlaceDetailData[0]?.is_delivery_or_takeout_available}
+                </span>
+                <span>{yapuPlaceDetailData[0]?.info}</span>
+              </p>
+              {!yapuPlaceDetailData[0]?.inside_stadium &&
+                selectedStadium &&
+                yapuPlaceDetailData[0] && (
+                  <KakaoMap
+                    place={yapuPlaceDetailData[0]!}
+                    stadium={selectedStadium!}
+                  />
+                )}
+            </section>
+            <hr className="w-[100%] border-b-1 border-dashed border-gray-400 " />
+            <section className="mt-[40px]">
+              {!isMenuLoading && (
+                <div className=" flex flex-col items-center ">
+                  <p className="text-center font-paper_logy text-[26px] mb-[20px]">
+                    📋추천 메뉴
+                  </p>
+                  <ul className=" w-[70%] border-b-2  pt-[40px] pr-[20px] pl-[20px] pb-[20px] mb-[20px] rounded-lg text-[#3d211a] bg-[#fff2d2]">
+                    {recommendedMenusData.map((i) => (
+                      <li
+                        key={i.menu_name}
+                        className="border-b-[1px] border-dashed border-[#3d211a]  flex flex-col mb-[20px] pb-[5px] text-[18px] text-bold "
+                      >
+                        <span className="font-s_core">{i.menu_name}</span>
+                        <span className="ml-auto mt-[10px] font-s_core">
+                          {i.price}
+                        </span>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              )}
+            </section>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
--- src/app/stadium/[id]/[place]/page.tsx
+++ src/app/stadium/[id]/[place]/page.tsx
@@ -1,28 +1,17 @@
 "use client";
 
 import "../../../globals.css";
-import KakaoMap from "@/components/KakaoMap";
-import { useRecommededMenusData } from "@/app/hooks/useRecommendedMenusData";
-import { useYapuPlaceDatailData } from "@/app/hooks/useYapuPlaceData";
-import { useStadiumStore } from "@/stores/stadium-store";
-import { usePlaceStore } from "@/stores/place-store";
-import BackButton from "@/components/BackButton";
 import { useEffect } from "react";
-import { usePathname, useRouter } from "next/navigation";
+import { usePathname } from "next/navigation";
 import { useState } from "react";
 import { useRef } from "react";
 import CommentsList from "@/components/CommentsList";
 import { getComments } from "@/app/api/comments";
+import PlaceInfo from "./PlaceInfo";
 
 export default function PlacePage() {
-  const { selectedStadium } = useStadiumStore();
-  const { selectedPlace, clearSelectedPlace } = usePlaceStore();
-
   const commentRef = useRef<HTMLInputElement>(null);
-  const router = useRouter();
-  const [isClickedBack, setIsClickedBack] = useState(false);
   const path = usePathname();
-  const stadiumId = decodeURI(path).split("/")[2];
   const placename = decodeURI(path).split("/")[3];
   const [comment, setComment] = useState("");
 
@@ -35,20 +24,6 @@ export default function PlacePage() {
     }
   };
 
-  const { isPlaceLoading, yapuPlaceDetailData } = useYapuPlaceDatailData(
-    (selectedStadium?.id as string) || stadiumId,
-    selectedPlace! || placename
-  );
-
-  const { isMenuLoading, recommendedMenusData } = useRecommededMenusData(
-    selectedPlace! || placename
-  );
-
-  const handleBackButton = () => {
-    clearSelectedPlace();
-    router.replace(`/stadium/${selectedStadium?.id}`);
-  };
-
   const onChangeComment = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (commentRef.current !== null) {
       // if (!loggedInUser) {
@@ -59,84 +34,13 @@ export default function PlacePage() {
     setComment(e.target.value);
   };
 
-  // 브라우저 뒤로가기 제어
-  useEffect(() => {
-    const handlePopState = () => {
-      console.log("뒤로가기 감지");
-      setIsClickedBack(true);
-      clearSelectedPlace();
-    };
-    window.addEventListener("popstate", handlePopState);
-    setIsClickedBack(false);
-  }, [isClickedBack]);
-
   useEffect(() => {
     handleGetComments(placename);
   }, []);
 
-  if (isPlaceLoading || isMenuLoading) {
-    return <div>loading...</div>;
-  }
-
   return (
     <div className="w-[70%] mt-[20px]">
-      <BackButton fn={handleBackButton} />
-      <div>
-        {yapuPlaceDetailData[0] && (
-          <div className="flex flex-col">
-            <section className="mb-[20px]">
-              <div>
-                <h1 className="flex flex-row justify-center items-center font-paper_logy text-[32px] text-center">
-                  <span className="mr-[5px]">
-                    {yapuPlaceDetailData[0]?.food_type}
-                  </span>
-                  <span>{yapuPlaceDetailData[0]?.name}</span>
-                </h1>
-              </div>
-              <p className="font-s_core font-bold text-[18px] mt-[30px] mb-[10px]">
-                위치 : {yapuPlaceDetailData[0]?.location}
-              </p>
-              <p className="font-s_core flex text-[18px] flex-row items-center mb-[10px]">
-                <span className="mr-[20px]">
-                  {yapuPlaceDetailData[0]?.is_delivery_or_takeout_available}
-                </span>
-                <span>{yapuPlaceDetailData[0]?.info}</span>
-              </p>
-              {!yapuPlaceDetailData[0]?.inside_stadium &&
-                selectedStadium &&
-                yapuPlaceDetailData[0] && (
-                  <KakaoMap
-                    place={yapuPlaceDetailData[0]!}
-                    stadium={selectedStadium!}
-                  />
-                )}
-            </section>
-            <hr className="w-[100%] border-b-1 border-dashed border-gray-400 " />
-            <section className="mt-[40px]">
-              {!isMenuLoading && (
-                <div className=" flex flex-col items-center ">
-                  <p className="text-center font-paper_logy text-[26px] mb-[20px]">
-                    📋추천 메뉴
-                  </p>
-                  <ul className=" w-[70%] border-b-2  pt-[40px] pr-[20px] pl-[20px] pb-[20px] mb-[20px] rounded-lg text-[#3d211a] bg-[#fff2d2]">
-                    {recommendedMenusData.map((i) => (
-                      <li
-                        key={i.menu_name}
-                        className="border-b-[1px] border-dashed border-[#3d211a]  flex flex-col mb-[20px] pb-[5px] text-[18px] text-bold "
-                      >
-                        <span className="font-s_core">{i.menu_name}</span>
-                        <span className="ml-auto mt-[10px] font-s_core">
-                          {i.price}
-                        </span>
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-              )}
-            </section>
-          </div>
-        )}
-      </div>
+      <PlaceInfo />
       <hr />
       <div className="mt-[20px]">
         <div className="flex flex-col">
