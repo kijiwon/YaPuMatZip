1e976995b96036fce3fa65bd583701a9c5ccef1d
kijiwon <kiji1313@gmail.com>
2025-01-12 19:24:41 +0900

    외부 이미지 허용

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   env: {
     NEXT_PUBLIC_KAKAO_API_KEY:process.env.NEXT_PUBLIC_KAKAO_API_KEY
+  },
+  images: {
+    remotePatterns: [{
+      protocol:'https',
+      hostname: '**'
+    },{
+      protocol:'http',
+      hostname:'**'
+    }]
   }
 };
 
