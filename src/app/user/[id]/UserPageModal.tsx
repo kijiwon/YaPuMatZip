069da245731282f93806d94100cdbe93d34dfab3
kijiwon <kiji1313@gmail.com>
2025-01-12 22:05:18 +0900

    user-page 모달 스타일링

--- src/app/user/[id]/UserPageModal.tsx
+++ src/app/user/[id]/UserPageModal.tsx
@@ -8,8 +8,17 @@ export default function UserPageModal() {
   useEffect(() => {
     setTimeout(() => {
       router.push("/");
-    }, 1500);
+    }, 1000);
   }, [router]);
 
-  return <div>접근 권한이 없습니다</div>;
+  return (
+    <div className="w-[100%] h-[100%] relative flex justify-center pt-[20px] bg-gray-200">
+      <div className="absolute flex flex-col justify-center items-center px-[120px] py-[80px] bg-white rounded-[15px] shadow-md font-paper_logy text-[22px]">
+        <p>접근 권한이 없습니다!</p>
+        <span className="text-[14px] mt-[10px]">
+          잠시 후 시작 페이지로 이동합니다...
+        </span>
+      </div>
+    </div>
+  );
 }
