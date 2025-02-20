40a5302ff5e06c1136e91474f6e3d981613e242a
kijiwon <kiji1313@gmail.com>
2025-02-21 00:42:40 +0900

    voicesearch stop button 수정

--- src/components/VoiceSearch.tsx
+++ src/components/VoiceSearch.tsx
@@ -52,7 +52,7 @@ export default function Record({ setTerms }: Props) {
       {listening ? (
         <FaStop
           onClick={onClickStopVoice}
-          className="cursor-pointer lg:text-[25px] text-[16px] "
+          className="cursor-pointer lg:text-[25px] text-[20px] lg:m-0"
         />
       ) : (
         <MdKeyboardVoice
