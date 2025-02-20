b9e7f2763a76c468cdeffe0db1506463bf16e5d1
kijiwon <kiji1313@gmail.com>
2025-02-21 00:28:03 +0900

    stadium tips 모바일 스타일링 추가

--- src/app/stadium/(with_header)/[id]/PlaceHeader.tsx
+++ src/app/stadium/(with_header)/[id]/PlaceHeader.tsx
@@ -12,10 +12,10 @@ export default function PlaceHeader() {
   }
 
   return (
-    <div className="lg:w-[800px] font-kbo mb-[30px] flex lg:flex-row flex-col lg:justify-between">
+    <div className="lg:w-[800px] font-kbo mb-[30px] flex lg:flex-row flex-col lg:justify-between border-b-2 border-dashed">
       <div>
         <p
-          className={`lg:text-[26px] text-[24px] tracking-wider text-${selectedStadium?.team_short_color[0]}-main`}
+          className={`lg:text-[26px] text-[20px] tracking-wider text-${selectedStadium?.team_short_color[0]}-main`}
         >
           {selectedStadium?.teams[0]}
           {selectedStadium?.teams.length == 2 && (
@@ -29,7 +29,7 @@ export default function PlaceHeader() {
             </>
           )}
         </p>
-        <p className="mt-[10px] lg:text-[18px] text-[16px]">
+        <p className="mt-[10px] lg:text-[18px] text-[14px] mb-[6px]">
           구장: {selectedStadium?.name}
         </p>
       </div>
@@ -37,19 +37,19 @@ export default function PlaceHeader() {
       {selectedStadium.tips?.map((tip, idx) => (
         <div
           key={idx}
-          className="h-fit font-roboto text-[14px] border-2 rounded-md py-2 px-4 border-main-blue border-dashed"
+          className="h-full font-roboto lg:text-[14px] text-[12px] lg:border-l-2 lg:pl-3 mt-[4px] lg:self-end"
         >
           {tip.tip && (
             <div>
-              <span className="text-main-red font-paper_logy mb-[4px] mr-[6px] text-[16px]">
+              <span className="text-main-red font-paper_logy mb-[4px] mr-[6px] lg:text-[16px]">
                 TIP!
               </span>
               <span>{tip.tip}</span>
             </div>
           )}
-          <div>
+          <div className="mb-[6px]">
             <div className="flex flex-row items-center">
-              <MdDeliveryDining size={20} />
+              <MdDeliveryDining className="text-[14px] lg:text-[20px]" />
               <span className="font-paper_logy tracking-wider">배달ZONE</span>
             </div>
             {tip.delivery_zone?.map((i, idx) => <p key={idx}>{i}</p>)}
