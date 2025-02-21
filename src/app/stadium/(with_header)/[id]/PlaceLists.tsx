0f90980f7d151b1f895b52dea5b61ee4ace2af60
kijiwon <kiji1313@gmail.com>
2025-02-21 15:55:24 +0900

    category selector 구현 완료

--- src/app/stadium/(with_header)/[id]/PlaceLists.tsx
+++ src/app/stadium/(with_header)/[id]/PlaceLists.tsx
@@ -50,18 +50,21 @@ export default function PlaceLists({ userId }: { userId: string }) {
   }
 
   return (
-    <div className="flex flex-col">
-      <div>
-        <select onChange={onChangeCategory} defaultValue={"all"}>
-          <option value="all">🍽️ 전체보기</option>
-          <option value="KOR">🍚 한식(분식/육류 등)</option>
-          <option value="JPN">🍣 일식(초밥/회/타코야끼 등)</option>
-          <option value="CHN">🍤 중식(중회요리/크림새우 등)</option>
-          <option value="WST">🍔 양식(피자/치킨/맥시칸 등)</option>
-          <option value="DSR">🍦 디저트</option>
-          <option value="BEV">🥤 음료</option>
-        </select>
-      </div>
+    <div className="lg:w-[800px] flex flex-col">
+      <select
+        onChange={onChangeCategory}
+        defaultValue={"all"}
+        className="cursor-pointer lg:self-end lg:border-0 lg:border-b-4 border-2 lg:rounded-none rounded-md border-blue-200 pl-2 pb-[2px] my-[14px] lg:w-fit outline-none lg:text-[16px] text-[14px] font-s_core_bold"
+      >
+        <option value="all">🍽️ 전체보기</option>
+        <option value="KOR">🍚 한식(분식/육류 등)</option>
+        <option value="JPN">🍣 일식(초밥/회/타코야끼 등)</option>
+        <option value="CHN">🍤 중식(중회요리/크림새우 등)</option>
+        <option value="WST">🍔 양식(피자/치킨/맥시칸 등)</option>
+        <option value="DSR">🍦 디저트</option>
+        <option value="BEV">🥤 음료</option>
+      </select>
+
       {selectedStadium?.id === "baseball-dream-park" ? (
         <div className="font-paper_logy text-center tracking-wider">
           <h1 className="text-[30px] text-main-red mb-[20px]">
@@ -73,6 +76,11 @@ export default function PlaceLists({ userId }: { userId: string }) {
         </div>
       ) : (
         <>
+          {filteredPlace.length === 0 && (
+            <p className="text-[20px] font-s_core">
+              해당 카테고리에 대한 장소가 없습니다 ˃̣̣̥ ᯅ ˂̣̣̥
+            </p>
+          )}
           <ul>
             {filteredPlace.map((place) => (
               <PlaceItem
