e1d9e264332cda9827e0b28c040a358379ab60ef
kijiwon <kiji1313@gmail.com>
2025-07-27 23:57:46 +0900

    Lazy Loading

--- src/app/stadium/(with_header)/[id]/page.tsx
+++ src/app/stadium/(with_header)/[id]/page.tsx
@@ -1,9 +1,11 @@
 import { createServerSideClientRSC } from "@/app/utils/server";
 import "../../../globals.css";
 import PlaceHeader from "./PlaceHeader";
-import PlaceLists from "./PlaceLists";
-import { Suspense } from "react";
+// import PlaceLists from "./PlaceLists";
+import React, { Suspense } from "react";
 import Loading from "./loading";
+
+const LazyPlaceLists = React.lazy(() => import("./PlaceLists"));
 
 export default async function Page() {
   const supabase = await createServerSideClientRSC();
@@ -15,7 +17,7 @@ export default async function Page() {
     <div className=" mt-[20px] lg:w-[70%] w-[90%] flex flex-col lg:items-center">
       <Suspense fallback={<Loading />}>
         <PlaceHeader />
-        <PlaceLists userId={user?.id as string} />
+        <LazyPlaceLists userId={user?.id as string} />
       </Suspense>
     </div>
   );
