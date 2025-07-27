5a473446d83b08b90e9e77434bb084e517d9e3af
kijiwon <kiji1313@gmail.com>
2025-07-28 00:10:07 +0900

    api 요청 오류 수정

--- src/app/api/ballpark-info.ts
+++ src/app/api/ballpark-info.ts
@@ -2,7 +2,7 @@
 
 import { createSupabaseBrowserClient } from "../utils/client/supabase"
 
-export const getBallparkInfo = async(id:string)=>{
+export const getBallparkInfo = async(id:number)=>{
     const supabase = createSupabaseBrowserClient();
     const result = await supabase.from('ballpark-info').select('*').eq('id',id);
 
