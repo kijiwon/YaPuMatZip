8a5a95e890d5a18854a373a93e3d6611011b6c78
kijiwon <kiji1313@gmail.com>
2025-09-04 13:54:18 +0900

    최적화-접근성 개선(tag 수정

--- src/app/user/[id]/loading.tsx
+++ src/app/user/[id]/loading.tsx
@@ -1,7 +1,7 @@
 export default function Loading() {
   return (
-    <div className="flex justify-center mt-[20px] ">
-      <div className="w-16 h-16 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
-    </div>
+    <ul className="flex justify-center mt-[20px] ">
+      <li className="w-16 h-16 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></li>
+    </ul>
   );
 }
--- src/components/ItemSkeleton.tsx
+++ src/components/ItemSkeleton.tsx
@@ -1,5 +1,5 @@
 export const ItemSkeleton = () => {
   return (
-    <div className="lg:w-[800px] w-[100%] lg:h-[80px] h-[60px] mb-[20px] bg-gray-200 rounded-xl"></div>
+    <li className="lg:w-[800px] w-[100%] lg:h-[80px] h-[60px] mb-[20px] bg-gray-200 rounded-xl"></li>
   );
 };
